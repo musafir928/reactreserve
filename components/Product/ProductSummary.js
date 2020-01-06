867a01a64b9c905a50ba4e63516860a5827c8c7e
Adil <uganoghli@gmail.com>
2020-01-06 16:41:42 +0100

    product detail page

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,5 +1,24 @@
-function ProductSummary() {
-  return <>ProductSummary</>;
+import { Item, Label } from "semantic-ui-react";
+import AddProductToCArt from "./AddProductToCart";
+
+function ProductSummary({ name, mediaUrl, _id, price, sku }) {
+  return (
+    <Item.Group>
+      <Item>
+        <Item.Image size='medium' src={mediaUrl} />
+        <Item.Content>
+          <Item.Header>{name}</Item.Header>
+          <Item.Description>
+            <p>€{price}</p>
+            <Label>SKU:{sku}</Label>
+          </Item.Description>
+          <Item.Extra>
+            <AddProductToCArt productId={_id} />
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </Item.Group>
+  );
 }
 
 export default ProductSummary;
--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,22 @@
-function Product() {
-  return <>product</>;
+import axios from "axios";
+import ProductSummary from "../components/Product/ProductSummary";
+import ProductAttributes from "../components/Product/ProductAttributes";
+
+function Product({ product }) {
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
 }
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+  const url = `http://localhost:3000/api/product`;
+  // next line: to make query string without adding ? mark and params to the url
+  const payload = { params: { _id } };
+  const res = await axios.get(url, payload);
+  return { product: res.data };
+};
 
 export default Product;
