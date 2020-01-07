deb0e805b5368e61a6b42717c0e54b86b59a05c5
Adil <uganoghli@gmail.com>
2020-01-07 20:30:22 +0100

    jwt secret

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://musafir928:000sun1016@reactreserve-y3yeb.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "asdfgh",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dlqbkzezu/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
