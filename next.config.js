a2a5828c3647f27b7b0193eb87c909a2f54f7eb9
Adil <uganoghli@gmail.com>
2020-01-09 00:57:01 +0100

    env files

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
       "mongodb+srv://musafir928:000sun1016@reactreserve-y3yeb.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "asdfgh",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dlqbkzezu/image/upload",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    STRIPE_SECRET_KEY: "sk_test_z9mpp3cVKBUGfPQE9W0U80AX00wJLoJEVn"
   }
 };
